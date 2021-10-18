69187d5d976523be999da9a823d773bcd0c52f84
pooya.oa <pooya.oladazimi@tib.eu>
2021-10-18 13:58:02 +0200

    move the plugin to a new extension

---
+++ ckanext/organization_group/plugin.py
@@ -0,0 +1,36 @@
+import ckan.plugins as plugins
+import ckan.plugins.toolkit as toolkit
+from flask import Blueprint
+from ckanext.organization_group.controllers import GroupOwnershipController
+
+
+
+class OrganizationGroupPlugin(plugins.SingletonPlugin):
+    plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.IBlueprint)
+
+    # IConfigurer
+
+    def update_config(self, config_):
+        toolkit.add_template_directory(config_, 'templates')
+        toolkit.add_public_directory(config_, 'public/statics')
+        toolkit.add_resource('public/statics', 'ckanext-organization-group')
+
+    def get_blueprint(self):
+
+        blueprint = Blueprint(self.name, self.__module__)        
+        blueprint.add_url_rule(
+            u'/upgrade_dataset/add_ownership_view/<id>',
+            u'add_ownership_view',
+            GroupOwnershipController.add_ownership_view,
+            methods=['GET']
+            )
+        
+        blueprint.add_url_rule(
+            u'/upgrade_dataset/save_ownership',
+            u'save_ownership',
+            GroupOwnershipController.save_ownership,
+            methods=['POST']
+            )
+
+        return blueprint

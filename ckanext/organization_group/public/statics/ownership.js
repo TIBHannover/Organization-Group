f4d0ae3b2973553a59644a70728ac85a4db332fc
pooya.oa <pooya.oladazimi@tib.eu>
2022-03-17 12:23:20 +0100

    hide other group dropdowns

--- ckanext/organization_group/public/statics/ownership.js
+++ ckanext/organization_group/public/statics/ownership.js
@@ -1,21 +1,15 @@
 $(document).ready(function(){
-    $('.org_dropdown').select2();
-    $('.group_dropdown').select2();
+    $('select.org_dropdown').select2();
+    $('select.group_dropdown').select2();
+    $('#groups_dropdown_box_1').show();
     $('#ownership-form').submit(function(e){
         $('#org_group_error').hide();
-        $('.group_dropdown').css('border', '');
         $('.org_dropdown').css('border', '');
         let org = $('#org_dropdown').find(':selected').val();
-        let group = $('#groups_dropdown').find(':selected').val();
         if(org == '0'){
             e.preventDefault();
             $('#org_group_error').show();
             $('.org_dropdown').css('border', '2px solid red');
-        } 
-        if(group == '0'){
-            e.preventDefault();
-            $('#org_group_error').show();
-            $('.group_dropdown').css('border', '2px solid red');
-        }          
+        }      
     });
 });

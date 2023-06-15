fa89199a5f071fe76660a4e8f456d5c8788411a4
pooya.oa <pooya.oladazimi@tib.eu>
2023-06-15 14:19:00 +0200

    added loading animation for next step button

--- ckanext/organization_group/public/statics/ownership.js
+++ ckanext/organization_group/public/statics/ownership.js
@@ -1,10 +1,12 @@
 $(document).ready(function(){
     $('select.org_dropdown').select2();
     $('#ownership-form').submit(function(e){
+        $('#next-step-loadin-animation').css('display', 'inline-block');
         $('#org_group_error').hide();
         $('.org_dropdown').css('border', '');
         let org = $('#org_dropdown').find(':selected').val();
         if(org == '0'){
+            $('#next-step-loadin-animation').css('display', 'none');
             e.preventDefault();
             $('#org_group_error').show();
             $('.org_dropdown').css('border', '2px solid red');
